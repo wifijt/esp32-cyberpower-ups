8aeef9378c0018946eeaed4cdaf3de81f9395bbc
wifijt <euchre@gmail.com>
2025-12-27 11:46:54 -0500

    revert to working

--- components/cyberpower_ups/sensor.py
+++ components/cyberpower_ups/sensor.py
@@ -1,14 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_WATT, UNIT_VOLT_AMPS, UNIT_PERCENT, DEVICE_CLASS_BATTERY, DEVICE_CLASS_POWER, ICON_TIMER
+from esphome.const import UNIT_WATT, UNIT_VOLT_AMPS, UNIT_PERCENT, DEVICE_CLASS_BATTERY, DEVICE_CLASS_POWER
 from . import CyberPowerUPS, CONF_ID
 
 CONF_WATTS = "watts"
 CONF_VA = "va"
 CONF_LOAD = "load"
 CONF_BATTERY = "battery"
-CONF_RUNTIME = "runtime" # Added
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.use_id(CyberPowerUPS),
@@ -16,7 +15,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_VA): sensor.sensor_schema(unit_of_measurement=UNIT_VOLT_AMPS, accuracy_decimals=0),
     cv.Optional(CONF_LOAD): sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, accuracy_decimals=0),
     cv.Optional(CONF_BATTERY): sensor.sensor_schema(unit_of_measurement=UNIT_PERCENT, accuracy_decimals=0, device_class=DEVICE_CLASS_BATTERY),
-    cv.Optional(CONF_RUNTIME): sensor.sensor_schema(unit_of_measurement="min", icon=ICON_TIMER, accuracy_decimals=0), # Added
 })
 
 async def to_code(config):
@@ -33,6 +31,3 @@ async def to_code(config):
     if CONF_BATTERY in config:
         sens = await sensor.new_sensor(config[CONF_BATTERY])
         cg.add(hub.set_battery_sensor(sens))
-    if CONF_RUNTIME in config: # Added
-        sens = await sensor.new_sensor(config[CONF_RUNTIME])
-        cg.add(hub.set_runtime_sensor(sens))
