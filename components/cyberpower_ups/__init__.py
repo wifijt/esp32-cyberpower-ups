d9774f102ee262e4837f3f51ac820187dac8f429
wifijt <euchre@gmail.com>
2025-12-27 10:40:39 -0500

    Update __init__.py

--- components/cyberpower_ups/__init__.py
+++ components/cyberpower_ups/__init__.py
@@ -2,17 +2,21 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['esp32']
-AUTO_LOAD = ['sensor', 'binary_sensor']
+# Define the key
+CONF_MAX_WATTS = "max_watts"
 
-# Create the C++ namespace
 cyberpower_ns = cg.esphome_ns.namespace('cyberpower_ups')
 CyberPowerUPS = cyberpower_ns.class_('CyberPowerUPS', cg.PollingComponent)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CyberPowerUPS),
-}).extend(cv.polling_component_schema('10s'))
+    # Allow the user to set max_watts, defaulting to 810.0
+    cv.Optional(CONF_MAX_WATTS, default=810.0): cv.float_,
+}).extend(cv.polling_component_schema('1s'))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    # Pass the value from YAML to the C++ setter
+    cg.add(var.set_max_watts(config[CONF_MAX_WATTS]))
